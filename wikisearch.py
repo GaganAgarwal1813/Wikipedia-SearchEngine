48d862c2f13ffe3e6e5a09075f0222e74d98973c
GaganAgarwal1813 <agarwal.gagan1813@gmail.com>
2022-08-09 19:41:45 +0530

    Info Search function made

--- wikisearch.py
+++ wikisearch.py
@@ -122,11 +122,40 @@ def ext_search(query):
                 print(title_file.readline())
         # print(s)
 
+
+def info(query):
+    # print(query)
+    global stemmer
+    query = stemmer.stem(query)
+    field = 'i'
+    title_file = open("temp/title0.txt", "r")
+    title_tags = pickle.load(open("temp/title_pos.pickle", "rb"))
+    word_position = pickle.load(open("temp/wpos0.pickle", "rb"))
+    
+    body_idx = open("temp/info_box_idx0.txt", "r")
+   
+    if query in word_position and field in word_position[query]:
+        position = word_position[query]['i']
+       
+        body_idx.seek(position)
+        s = body_idx.readline()
+        
+        s = s.split(',')
+        for ele in s:
+            # print(ele)
+            if ele:
+                ele = ele.split(':')
+                title_file.seek(title_tags[int(ele[0])-1])
+                print(title_file.readline())
+        # print(s)
+
 def main():
     query = sys.argv[1]
+    query = query.lower()
     # title_test(query)
     # category_search(query)
-    ext_search(query)
+    # ext_search(query)
+    info(query)
 
 if __name__ == "__main__":
     main()
